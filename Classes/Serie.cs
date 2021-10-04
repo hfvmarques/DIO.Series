d68bb5fc306907af97c9b32104a56f80f43309be
Henrique Marques <hfvmarques@gmail.com>
2021-10-04 19:36:58 -0400

    Implementados métodos Listar e Inserir série

--- Classes/Serie.cs
+++ Classes/Serie.cs
@@ -9,6 +9,7 @@ namespace DIO.Series
     private string Titulo { get; set; }
     private string Descricao { get; set; }
     private int Ano { get; set; }
+    public bool Excluido { get; set; }
 
     public Serie(int id, Genero genero, string titulo, string descricao, int ano)
     {
@@ -17,6 +18,7 @@ namespace DIO.Series
       this.Titulo = titulo;
       this.Descricao = descricao;
       this.Ano = ano;
+      this.Excluido = false;
     }
 
     public override string ToString()
@@ -36,6 +38,10 @@ namespace DIO.Series
     public int retornaId()
     {
       return this.Id;
+    }
+    public void Excluir()
+    {
+      this.Excluido = true;
     }
   }
 }
